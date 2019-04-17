5cc7d52fc594fb86f368890e091c3d428716f1ab
Payam Rafiee <payamrafiee@Payams-Mac-mini-3.local>
2019-04-17 20:00:19 +0300

    ready to deploy

---
+++ build/precache-manifest.8936e515ff37d80dd0beb591f3ecfa95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "95731f8aabf002179e6a",
+    "url": "/static/js/main.ded8e162.chunk.js"
+  },
+  {
+    "revision": "ad79c86be2a09947cd49",
+    "url": "/static/js/2.b96bd03f.chunk.js"
+  },
+  {
+    "revision": "95731f8aabf002179e6a",
+    "url": "/static/css/main.f8953bda.chunk.css"
+  },
+  {
+    "revision": "ad79c86be2a09947cd49",
+    "url": "/static/css/2.f71617f5.chunk.css"
+  },
+  {
+    "revision": "fdbdd4be7bb449a15af09d05f7cd628f",
+    "url": "/index.html"
+  }
+];
